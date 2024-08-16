b4a74d2e851b4979d67f67d0ea123961222d6bf0
sesquivel <sesquivel13@hotmail.com>
2024-08-16 01:34:08 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Software para encriptar y desencriptar un texto
+
+El programa encripta según unas reglas, una palabra y/o un texto que sea ingresado en el campo "Texto para encriptar y desencriptar. 
+
+Sólo recibirá letras minúsculas, no acepta caracteres especiales, caracteres en mayúscula, ni caracteres con tilde.
+
+La interfaz cuenta con dos campos de texto, el primero se llama Texto para encriptar/desencriptar, en este espacio el usuario digitará la palabra y/o texto que desea encriptar, en el cuadro de texto resultado, se mostrara la palabra y/o texto encriptado o desencriptado, según haya elegido el usuario los botones en la parte inferior. 
+
+Adicional el programa cuenta con un botón de copiar, el cual copia el texto, que este en el momento, en el campo de texto Resultado, lo copiará al portapapeles de Windows, y el usuario podrá utilizar las teclas crtl+v para copiarlo en el campo de texto para encriptar/desencriptar, o donde prefiera.
+
+
+## Funcionalidad 🚀
+
+El programa realiza una encriptación, de acuerdo a unas reglas establecidas, lo que hacen es tomar la palabra y/o el texto, aplicarlas y mostrarlas en el campo de texto Resultado, el usuario puede copiar esta encriptación y copiarla donde guste, igualmente, puede copiarla en el campo texto para encriptar/desencriptar y volver a desencriptarlo.
+
+Las reglas para encriptar son las siguientes:
+
+"a" es convertido para "ai"
+"e" es convertido para "enter" 
+"i" es convertido para "imes"
+"o" es convertido para "ober"
+"u" es convertido para "ufat"
+Solo letras minúsculas
+No se permite caracteres con tilde
+
+### Como Usarlo📋
+
+Los usuarios pueden a través de esta interfaz, poner un texto en el campo texto para encriptar/desencriptar, encriptarlo, verá el texto encriptado en el campo de texto Resultado, copiarlo, pegarlo en el cuadro texto encriptar/desencriptar y volverlo a desencriptar.
+
+El usuario puede utilizar encriptación para su contraseñas que queden seguras, o para enviar mensajes seguros, entre otras cosas.
+
+Nota: el desencriptador solo podrá evaluar el texto y desencriptarlo, si este fue encriptado con las mismas reglas y se utilizó este mismo programa.
+
+## Construido con 🛠️
+
+El programa esta hecho en HTML, CSS y Javascript
+
+## Autor ✒️
+
+Sandra Milena Esquivel
+
---
+++ script.js
@@ -0,0 +1,123 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+
+/*  Funcion para verificar que el texto no contenga simbolos especiales, ni letras con acento, 
+    y llamar a la funcion para encriptar el texto */
+
+function botonEncriptar(){
+    const caracter = /^[a-z\s]+$/;
+
+    if (caracter.test(textArea.value)) {
+        const textEncriptado = encriptar(textArea.value);
+     //   console.log(textEncriptado);
+        mensaje.value = textEncriptado;
+            
+    } else{
+        alert("El campo no debe estar vacío, ni contener letras con acento, ni símbolos especiales.");
+
+    }    
+       // console.log(mensaje.value);
+    textArea.value="";
+    mensaje.style.backgroundImage = "none";
+}
+
+/* Reglas de encriptación:
+"a" es convertido para "ai"
+"e" es convertido para "enter" 
+"i" es convertido para "imes"
+"o" es convertido para "ober"
+"u" es convertido para "ufat"
+Solo letras minusculas
+No se permite caracteres con tilde
+*/
+
+// Funcion que encripta el texto según las reglas de encriptación
+
+function encriptar(stringEncriptado){
+
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"],["o", "ober"],["u", "ufat"]];
+    console.table(matrizCodigo);
+    //stringEncriptado = stringEncriptado.tolowerCase;
+    console.log(stringEncriptado);
+
+    for (let i= 0; i < matrizCodigo.length; i++){
+        console.log(matrizCodigo[i],0);
+        if (stringEncriptado.includes(matrizCodigo[i][0])){
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+            console.log(stringEncriptado);
+        } 
+    }
+    return stringEncriptado;
+        
+}
+
+/*  Funcion para verificar que el texto no contenga simbolos especiales, ni letras con acento, 
+    y llamar a la funcion para desencriptar el texto */
+
+function botonDesencriptar(){
+    const caracter = /^[a-z\s]+$/;
+    
+    if (caracter.test(textArea.value)) {
+        const textEncriptado = desEncriptar(textArea.value);
+      //  console.log(textEncriptado);
+        mensaje.value = textEncriptado;
+            
+    } else{
+        alert("El campo no debe estar vacío, ni contener letras con acento, ni símbolos especiales.");
+
+    }  
+   
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
+
+/*
+Reglas de desencriptación: 
+"enter" es convertido para "e" 
+"imes" es convertido para "i"
+"ai" es convertido para "a"
+"ober" es convertido para "o"
+"ufat" es convertido para "u"
+Solo letras minusculas
+No se permite acentuación de palabras    
+*/
+
+// Funcion que desencripta el texto según las reglas de desencriptación, verifica si el texto esta encriptado o no
+
+function desEncriptar(stringDesencriptado){
+
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"],["o", "ober"],["u", "ufat"]];
+    const encriptado = stringDesencriptado;
+    console.table(matrizCodigo);
+
+    for (let i= 0; i < matrizCodigo.length; i++){
+        if (stringDesencriptado.includes(matrizCodigo[i][1])){
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+
+    if (encriptado == stringDesencriptado ){
+        alert("El texto no esta encriptado");
+        textArea.value = "";
+    }    
+  
+    return stringDesencriptado
+}
+      
+    
+// Copia el texto en el campo de resultado al portapapeles.
+
+function copiar(){
+    const textDesencriptado = mensaje.value;
+    const boton = Document.getElementsByClassName("copiar");
+
+    if (textDesencriptado == "") {
+        alert("No hay texto para copiar.");
+    } else {
+        navigator.clipboard.writeText(textDesencriptado);
+        alert("Texto Copiado. Con Ctr+v puede pegarlo en el campo de texto para encriptar o desencriptar");
+        mensaje.value="";
+    }
+    
+}
